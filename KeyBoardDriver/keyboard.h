aea3e50d21fa93e48b4d6074a47529341a9f0466
supermanc88 <supermanc88@gmail.com>
2018-12-19 14:45:10 +0800

    添加查看按键信息

--- KeyBoardDriver/keyboard.h
+++ KeyBoardDriver/keyboard.h
@@ -6,11 +6,19 @@ extern "C"
 
 #include <ntddk.h>
 
+#include <ntddkbd.h>
+
 // 先绑定所有的设备
 NTSTATUS AttachAllDevice(PDRIVER_OBJECT DriverObject);
 
 // 通用的派遣函数
 NTSTATUS CommonDisPatch(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+
+// 读按键信息
+NTSTATUS ReadDisPatch(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+
+// 读完成函数
+NTSTATUS ReadCompletionRoutine(PDEVICE_OBJECT DeviceObject, PIRP Irp, PVOID Context);
 
 #ifdef __cplusplus
 }
